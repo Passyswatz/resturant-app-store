cf11cb51dee06b376a715dbbe710d6f78f8b39fa
passyswatz <passyswatz@gmail.com>
2023-08-06 06:29:32 +0100

    mobile bar

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-  // Get references to the menu icon and the menus
-  const menuIcon = document.getElementById('menu-button');
-  const navbarLinks = document.querySelector('.navbar-menus');
 
-  // Function to toggle the visibility of the menus
-  function toggleMenus() {
-      navbarLinks.classList.toggle('active');
-  }
 
-  // Add a click event listener to the menu icon
-  menuIcon.addEventListener('click', toggleMenus);
+   // Get references to the menu icon and the menus
+   const menuIcon = document.getElementById('menu-button');
+   const menus = document.querySelector('.menus-mobile');
+ 
+   
+   // Function to toggle the visibility of the menus
+   function toggleMenus() {
+     menus.classList.toggle('hidden');
+ }
+ 
+   // Add a click event listener to the menu icon
+   menuIcon.addEventListener('click', toggleMenus);
