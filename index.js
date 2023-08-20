919ad5b22bb40012a494847738bf30c6bdd6fb2f
passyswatz <passyswatz@gmail.com>
2023-08-20 11:28:49 +0100

    finalizing

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 
 
-   // Get references to the menu icon and the menus
-   const menuIcon = document.getElementById('menu-button');
-   const menus = document.querySelector('.menus-mobile');
- 
-   
-   // Function to toggle the visibility of the menus
-   function toggleMenus() {
-     menus.classList.toggle('hidden');
- }
- 
-   // Add a click event listener to the menu icon
-   menuIcon.addEventListener('click', toggleMenus);
+document.addEventListener("DOMContentLoaded", function () {
+    const menuButton = document.getElementById("menu-button");
+    const mobileMenu = document.getElementById("mobile-menu");
+  
+    menuButton.addEventListener("click", function () {
+      mobileMenu.classList.toggle("active");
+    });
+  });
 
 
    // Function to open the right sidebar
@@ -235,3 +231,7 @@ function sixthElement() {
         plus.style.display = "block"
     }
 }
+
+
+
+
