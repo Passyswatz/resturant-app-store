71cde19a66b7ff9a320fba89e140cc909dc86dd4
passyswatz <passyswatz@gmail.com>
2023-08-25 04:09:56 +0100

    finale

--- burger.js
+++ burger.js
@@ -1,5 +1,5 @@
 // JavaScript
-fetch('https://fakestoreapi.com/products/')
+fetch('/')
   .then((data) => {
     return data.json();
   })
@@ -20,3 +20,44 @@ fetch('https://fakestoreapi.com/products/')
       </div>`;
     });
     document.getElementById('card').innerHTML = data1;
+
+    // Initialize cart object to store selected items and their quantities
+    const cart = {};
+
+    // Get the cart container outside the loop to avoid redefinition
+    const cartContainer = document.getElementById('cart-container');
+
+    // Function to update cart display
+    const updateCartDisplay = () => {
+      // Update the cart sidebar and display selected items
+      cartContainer.innerHTML = '';
+
+      let total = 0;
+
+      for (const cartItemId in cart) {
+        const cartItemData = cart[cartItemId];
+        const cartItem = document.createElement('div');
+        cartItem.classList.add('cart-item');
+        cartItem.setAttribute('data-item-id', cartItemId);
+        cartItem.innerHTML = `
+          <div class="cart-image">
+            <img src="${cartItemData.item.image}" alt="cart-img" class="cart-img"/>
+          </div>
+          <div class="cart-details">
+            <h2>${cartItemData.item.title}</h2>
+            <p>${cartItemData.item.price} usd</p>
+            <button class="remove-button">remove</button>
+          </div>
+          <input class="item-quantity" type="number" value="${cartItemData.quantity}" min="1" data-cart-item-id="${cartItemId}"/>
+        `;
+
+        cartContainer.appendChild(cartItem);
+
+        // Update the total by adding the price of the current item multiplied by its quantity
+        total += cartItemData.item.price * cartItemData.quantity;
+      }
+
+      // Calculate and display subtotal
+      const subtotalAmount = document.getElementById('subtotal-amount');
+      subtotalAmount.textContent = `$${total.toFixed(2)}`;
+    };
